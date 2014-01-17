0e389828fb9c43745a0aef5071749b924f296ebd
joe <joseph.beuckman@usbank.com>
2014-01-17 11:51:36 -0600

    generating curve descriptions for each range

--- spec/randomClusterPoints.spec.js
+++ spec/randomClusterPoints.spec.js
@@ -1,15 +1,27 @@
 
-var rcp = require('../lib/randomClusterPoints');
 
 describe("Random Cluster Points", function() {
 
-    it("generates bell curve descriptions for each dimension", function() {
+    var ranges = [[0,1],[2,3],[4,5]];
+    var rcp = require('../lib/randomClusterPoints');
 
-        var ranges = [[0,1],[2,3],[4,5]];
+    beforeEach(function() {
+
+        rcp.init(ranges, 3);
+    });
+
+    it("generates bell curve descriptions for each dimension", function() {
 
         var curves = rcp.init(ranges, 3);
 
         expect(curves.length).toEqual(3);
+    });
+
+    it("generates a point", function() {
+
+        var point = rcp.generatePoint();
+
+        expect(point.length).not.toBeNull();
     });
 
 });
