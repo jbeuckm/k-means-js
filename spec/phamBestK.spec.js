75916d263f98698db8a68431c455965f9ff9b6a0
joe beuckman <joebeuckman@gmail.com>
2014-01-19 20:57:19 -0600

    find best k algorithm and tests

--- spec/phamBestK.spec.js
+++ spec/phamBestK.spec.js
@@ -17,11 +17,7 @@ describe("Find best value of K", function() {
 
     var means = [[0,0]];
 
-    function badK() {
-      return pbk.alpha(means, 1);
-    }
-
-    expect(badK).toThrow();
+    expect(pbk.alpha(means, 1)).toBeUndefined();
 
     means.push([1,1]);
 
@@ -31,20 +27,69 @@ describe("Find best value of K", function() {
 
     expect(pbk.alpha(means, 2)).toEqual(2 + (1-2)/6);
 
-
   });
 
 
   it("calculates f(K)", function(){
 
-    // two obvious clusters
-    var points = [ [0,0],[0,1],[1,0], [9,10],[10,9],[10,10] ];
-    var means = [[0,0]];
-    var assignments = [0,1];
+    var kmeans = require('../lib/kmeans');
 
-    var f_1 = pbk.f(points, means, assignments);
+    function bell(center, width) {
+      var a = center + width * (-.5 + Math.random());
+      var b = center + width * (-.5 + Math.random());
+      var c = center + width * (-.5 + Math.random());
+      return (a + b + c) / 3;
+    }
 
-    expect(f_1).toEqual(1);
+    // three obvious clusters
+    var points = [];
+    for (var i=0; i<10; i++) {
+      points.push([
+        bell(.95,.1),
+        bell(.05,.1),
+        bell(.05,.1)
+      ]);
+      points.push([
+        bell(.05,.1),
+        bell(.95,.1),
+        bell(.05,.1)
+      ]);
+      points.push([
+        bell(.05,.1),
+        bell(.05,.1),
+        bell(.95,.1)
+      ]);
+    }
+
+    var result = {};
+    var pham = {};
+    var k = 1;
+
+    result[k] = kmeans.algorithm(points, k);
+    pham[k] = pbk.f(points, result[k].means, result[k].assignments);
+    console.log(k+" => "+pham[k].f);
+    expect(pham[k].f).toEqual(1);
+    k++;
+
+    while (k < 10) {
+      result[k] = kmeans.algorithm(points, k);
+      pham[k] = pbk.f(points, result[k].means, result[k].assignments, pham[k-1].alpha, pham[k-1].distortion);
+      console.log(k+" => "+pham[k].f);
+      k++;
+    }
+
+    var bestK;
+    var minF = Number.MAX_VALUE;
+    for (var i in pham) {
+      if (pham[i].f < minF) {
+        minF = pham[i].f;
+        bestK = pham[i].K;
+      }
+    }
+
+    expect(bestK).toEqual(3);
+
+    expect(pbk.findBestK(points, 10).bestK).toEqual(3);
 
   });
 
