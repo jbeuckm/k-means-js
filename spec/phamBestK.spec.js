520cb2ed9a2165bf2087c0acf31aaf7bf04a5a58
joe beuckman <joebeuckman@gmail.com>
2014-01-19 19:14:34 -0600

    established f(K)

--- spec/phamBestK.spec.js
+++ spec/phamBestK.spec.js
@@ -18,21 +18,34 @@ describe("Find best value of K", function() {
     var means = [[0,0]];
 
     function badK() {
-      return pbk.alpha_K(means, 1);
+      return pbk.alpha(means, 1);
     }
 
     expect(badK).toThrow();
 
     means.push([1,1]);
 
-    expect(pbk.alpha_K(means, 1)).toEqual(1-3/8);
+    expect(pbk.alpha(means, 1)).toEqual(1-3/8);
 
     means.push([2,2]);
 
-    expect(pbk.alpha_K(means, 2)).toEqual(2 + (1-2)/6);
+    expect(pbk.alpha(means, 2)).toEqual(2 + (1-2)/6);
 
 
   });
 
+
+  it("calculates f(K)", function(){
+
+    // two obvious clusters
+    var points = [ [0,0],[0,1],[1,0], [9,10],[10,9],[10,10] ];
+    var means = [[0,0]];
+    var assignments = [0,1];
+
+    var f_1 = pbk.f(points, means, assignments);
+
+    expect(f_1).toEqual(1);
+
+  });
 
 });
