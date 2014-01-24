4908acd73c92dda7ea6f4d84f61a414cd901590f
joe <joseph.beuckman@usbank.com>
2014-01-24 16:47:06 -0600

    establishing a data normalizer

---
+++ spec/dataset.spec.js
@@ -0,0 +1,33 @@
+
+var dataset = require('../lib/dataset');
+
+describe("Dataset", function() {
+
+    var data = [
+        {
+            category: 1,
+            value: 1
+        },
+        {
+            category: 2,
+            value: 50
+        },
+        {
+            category: 1,
+            value: 100
+        },
+        {
+            category: 1,
+            value: 75
+        }
+    ];
+
+    var params = {
+        category: "discrete"
+    };
+
+    it("normalizes a dataset", function() {
+
+    });
+
+});
